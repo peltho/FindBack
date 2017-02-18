8c6f32b8e0d79d33b392a2b6de750c11e6c8b6d3
peltho <boitutile@gmail.com>
2017-02-18 17:54:01 +0100

    WIP

--- src/FindBack/SiteBundle/Form/Type/WantedType.php
+++ src/FindBack/SiteBundle/Form/Type/WantedType.php
@@ -14,10 +14,7 @@ class WantedType extends AbstractType
             ->add('place', new PlaceType(), array('label' => false))
             ->add('date', new DateType(), array('label' => false))
             ->add('description', new DescriptionType(), array(/*'required' => false, */'label' => false))
-            ->add('circumstances', 'textarea', array('label' => false,
-                'attr' => array(
-                    'placeholder' => 'Comment vous êtes-vous rencontrés ?'
-                ),
+            ->add('circumstances', 'text', array('label' => false,
                 'required' => false
             ));
     }
